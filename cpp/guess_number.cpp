b16191e76afe5479cf06a2f6d652ef8d4fd87275
Justin K <justin.kolaras0@gmail.com>
2023-05-22 15:10:05 -0700

    File rename

---
+++ cpp/guess_number.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <random>
+
+int random(int start, int end) {
+    std::random_device rd;
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<> distribution(start, end);
+
+    return distribution(eng);
+}
+
+bool inRange(int value, int start, int end) {
+    return value >= start && value <= end;
+}
+
+int main() {
+    const int number = random(1, 100);
+
+    int guess = 0;
+    int attemptsRemaining = 5;
+
+    std::cout << "Welcome to guess the number! Please guess a number between 1 and 99.\n";
+    std::cout << attemptsRemaining << " attempts remaining.\n";
+
+    while (guess != number && attemptsRemaining > 0) {
+        if (!(std::cin >> guess)) {
+            std::cout << "Invalid input. Please enter a valid integer.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+
+        if (!inRange(guess, 1, 99)) {
+            std::cout << "Not in range.\n";
+            continue;
+        }
+
+        attemptsRemaining--;
+
+        // To stop additional dialogues.
+        if (guess == number || attemptsRemaining == 0) {
+			break;
+		}
+
+        if (guess > number) {
+            std::cout << "Lower.\n";
+        }
+        else if (guess < number) {
+            std::cout << "Higher.\n";
+        }
+
+        std::cout << attemptsRemaining << " attempt(s) remaining.\n";
+    }
+
+    if (guess == number) {
+        std::cout << "Congratulations! You guessed the number!\n";
+    }
+    else {
+        std::cout << "You lose! The number was " << number << ".\n";
+    }
+
+    return 0;
+}
